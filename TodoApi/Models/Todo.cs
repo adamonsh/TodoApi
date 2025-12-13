5d6dcfff0faa4d7c7efebdc6599106b39378f748
Adam Sheikh <adamosh91@gmail.com>
2025-12-12 23:09:13 -0500

    Completed tasks and passed acceptance cases outlined in issue T-102

--- TodoApi/Models/Todo.cs
+++ TodoApi/Models/Todo.cs
@@ -2,5 +2,7 @@ namespace TodoApi.Models;
 
 public class Todo
 {
-    
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public bool isComplete { get; set; } = false;
 }
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,3 +1,5 @@
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
